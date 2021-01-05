b6973d8a8a8f649c1bf04a935944baa09471eb02
Cotelea <marius.cotelea1003@gmail.com>
2021-01-05 21:27:51 +0200

    am adaugat a doua tema pentru acasa

---
+++ Tema 03.11.2020/scripts/script.js
@@ -0,0 +1,15 @@
+var a = -1;
+        let b = 24;
+        const c = 2;
+        for(var i=0;i<10;i++)
+        {
+            console.log(i);
+        }
+        console.log(i);
+function objectDisapear(){
+    document.getElementById("wow").style.display = "none";
+}
+function objectApear(){
+    document.getElementById("wow").style.display="block";
+    document.getElementById("wow").style.backgroundColor="lightblue";
+}
